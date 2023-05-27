afcac907f5575aef0745ee214ef1a9e1e7cfc60c
Beeru$ <77969007+Darshan0902@users.noreply.github.com>
2023-05-27 17:16:11 +0530

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -10,3 +10,14 @@ years = 2 #define the years
 balance_sheet = requests.get(f'https://financialmodelingprep.com/api/v3/balance-sheet-statement/{company}?limit={years}&apikey={api_key}')
 
 
+
+
+#merging and uploading values in a variable & Formatting it into a JSON format.
+
+balance_sheet = balance_sheet.json()
+
+# Uncomment the below line to check the values available in balance sheet;
+#print(balance_sheet)
+
+
+
