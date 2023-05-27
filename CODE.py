1b7e191db58743c4c07c68b5fb135e0a242b6bac
Beeru$ <77969007+Darshan0902@users.noreply.github.com>
2023-05-27 17:17:11 +0530

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -26,4 +26,9 @@ total_current_assets = balance_sheet[0]['totalCurrentAssets']
 print(f"Total current assets of  {company} : {total_current_assets:,}")
 
 
+#Calculating the Total Liablilites:
+total_current_liabilities = balance_sheet[0]['totalCurrentLiabilities']
+# Ensure that you have given proper index here in this case '0' specifies the value of the current most entry.
+print(f"Total current Liabilities of  {company} : {total_current_liabilities:,}")
+
 
