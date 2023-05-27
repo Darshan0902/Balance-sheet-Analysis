2a95408dc1d73cc73c31e1bb5db84dae17645ebc
Beeru$ <77969007+Darshan0902@users.noreply.github.com>
2023-05-27 17:14:59 +0530

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 api_key =""  #Generate yours from the site : https://site.financialmodelingprep.com/developer/docs/ will attach this in the README.MD as well.
 
 company = "GOOG"  # make sure you use the ticker symbol.
-
 years = 2 #define the years
 balance_sheet = requests.get(f'https://financialmodelingprep.com/api/v3/balance-sheet-statement/{company}?limit={years}&apikey={api_key}')
+
+
